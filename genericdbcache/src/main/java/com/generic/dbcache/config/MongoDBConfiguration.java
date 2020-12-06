930448cec441df30c3a47d3fc238161ff9de5d4f
Jaheer <Jaheer@LAPTOP-RMO3725H>
2020-12-06 21:18:56 +0530

    providing a way to access DAO class

--- genericdbcache/src/main/java/com/generic/dbcache/config/MongoDBConfiguration.java
+++ genericdbcache/src/main/java/com/generic/dbcache/config/MongoDBConfiguration.java
@@ -1,9 +1,7 @@
 package com.generic.dbcache.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.generic.dbcache.dao.IGenericDBCacheDAO;
@@ -12,41 +10,10 @@ import com.generic.dbcache.dao.mongo.MongoRepositoryCacheDAOImpl;
 import com.generic.dbcache.dao.mongo.MongoTemplateCacheDAOImpl;
 import com.generic.dbcache.value.AbstractValue;
 import com.generic.dbcache.value.GenericCollection;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
 @Configuration
 public final class MongoDBConfiguration {
-	
-	@Value("${mongodb.connection.uri}")
-	private String mongodbConnectionURI;
-	
-	@Value("${database.name}")
-	private String dataBaseName;
-	
-	@Primary
-	@Bean(name = "genericMongoClient")
-	public MongoClient genericMongoClient() {
-		final ConnectionString connectionString = new ConnectionString(mongodbConnectionURI);
-		final MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
-		return MongoClients.create(mongoClientSettings);
-	}
-
-	@Primary
-	@Bean(name = "genericMongoTemplate")
-	public MongoTemplate genericMongoTemplate() {
-		return new MongoTemplate(genericMongoClient(),dataBaseName);
-	}
-	
-	@Primary
-	@Bean(name = "genericMongoDatabase")
-	public MongoDatabase genericMongoDatabase() {
-		return genericMongoClient().getDatabase(dataBaseName);
-	}
 	
 	@Bean(name = "MongoRepositoryCacheDAO")
 	public <V extends AbstractValue> IGenericDBCacheDAO<String,V> mongoRepositoryCacheDAO(MongoRepository<GenericCollection<V>,String> repository) {
